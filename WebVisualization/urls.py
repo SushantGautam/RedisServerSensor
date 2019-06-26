1f7ca05b2227a5306aee2829a6d5ca926881539e
Sushant Gautam <susant.gautam@gmail.com>
2019-06-26 22:27:38 +0545

    added redis url

--- WebVisualization/urls.py
+++ WebVisualization/urls.py
@@ -1,1 +1,12 @@
-urlpatterns = []
+from django.shortcuts import render
+from django.urls import path
+
+
+def HomePage(request):
+    return render(request, 'homepage.html')
+
+
+
+urlpatterns = [
+path('', HomePage),
+]
