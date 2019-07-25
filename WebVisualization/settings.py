b456b723a3a9c752d928bce28b5d8aa049087b9b
Sushant Gautam <susant.gautam@gmail.com>
2019-07-25 15:23:55 +0545

    redis url update

--- WebVisualization/settings.py
+++ WebVisualization/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
 
 # REDIS_URL = os.environ['REDIS_URL']
 
-REDIS_URL = "redis://h:pb7db417bc4315578831fd6ee5f9728ef080116d100f144e403535878eb20a914@ec2-35-175-40-87.compute-1.amazonaws.com:29199"
+REDIS_URL = "redis://h:pb7db417bc4315578831fd6ee5f9728ef080116d100f144e403535878eb20a914@ec2-3-221-86-226.compute-1.amazonaws.com:9179"
 
 # REDIS_URL = "redis://127.0.0.1:6379"
 
